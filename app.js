a4e699e8f4fb57b982b91bd39a2c99d0c6e37678
KhaotiCreator <62815899+KhaotiCreator@users.noreply.github.com>
2023-09-15 17:52:20 +0100

    Moved image & animation
    Page Error:
    Shifted to the left, will need to fix that
    Description link needs to be moved under image

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        const elements = document.querySelectorAll(".slide-in");
+
+        elements.forEach(function (element) {
+            const rect = element.getBoundingClientRect();
+
+            if (rect.top < window.innerHeight && rect.bottom >= 0) {
+                element.classList.add("slide-in");
+            }
+        });
+    });
+});
